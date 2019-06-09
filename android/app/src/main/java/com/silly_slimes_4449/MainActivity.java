f59069390ef05a5cff472153f7a7ce9bd280e3c2
Crowdbotics <team@crowdbotics.com>
2019-06-09 21:35:25 +0000

    initial commit

---
+++ android/app/src/main/java/com/silly_slimes_4449/MainActivity.java
@@ -0,0 +1,15 @@
+package com.silly_slimes_4449;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "silly_slimes_4449";
+    }
+}
